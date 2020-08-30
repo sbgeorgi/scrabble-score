038241fff5ad652111762719b8d5c0ceba9dc6dc
Georgi Chertkov <georgichertkov@gmail.com>
2020-08-30 22:20:23 +0300

    commit

---
+++ scrabble-score.js
@@ -0,0 +1,42 @@
+//
+// This is only a SKELETON file for the 'Scrabble Score' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+const values = {
+  'a':1,
+  'b':3,
+  'c':3,
+  'd':2,
+  'e':1,
+  'f':4,
+  'g':2,
+  'h':4,
+  'i':1,
+  'j':8,
+  'k':5,
+  'l':1,
+  'm':3,
+  'n':1,
+  'o':1,
+  'p':3,
+  'q':10,
+  'r':1,
+  's':1,
+  't':1,
+  'u':1,
+  'v':4,
+  'w':4,
+  'x':8,
+  'y':4,
+  'z':10,
+  }
+  
+ export const score = (string = "") => {
+  let temp = 0;
+  
+  let placeHolder = string.toLowerCase();
+  for(let i=0; i<string.length; i++){
+    temp = temp + values[placeHolder.charAt(i)];
+  }
+  return temp;
+};
